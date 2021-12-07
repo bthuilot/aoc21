5127a1e88f9fec4d35dbf705ce0f4e069300bc1e
Bryce Thuilot <bthuilot@gmail.com>
2021-12-07 15:11:48 -0500

    Refactor of day 5, huge speed boost 🧑‍💻

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -11,7 +11,6 @@ import qualified Data.Text as T
 import qualified Data.Text.Read as R
 import Data.Either
 import Data.List
-import Debug.Trace
 
 -- | 'Point' represents an X,Y point 
 type Point = (Int, Int)
@@ -19,19 +18,15 @@ type Point = (Int, Int)
 -- the second 'Point' in the pair
 type Line = (Point, Point)
 
--- | 'Grid' represents the XY Plane where each 'Int' represents the amount of lines that cross that point
-type Grid = [[Int]]
-
 -- | 'runDay05' runs the 2 parts for Day 5 with the contents for a given file path
 -- https://adventofcode.com/2021/day/5
 runDay05 :: String -> Result
 runDay05 filename = do
   let input = unsafePerformIO $ readFileLinesText filename
-  let graphedLines = parseInput input
-  let initGrid = createGrid graphedLines
-  let (diag, nonDiag) = partition isDiag graphedLines
-  let (p1, grid) = countOverlap initGrid nonDiag -- Part 1: count only horizontal and vertical lines
-  let (p2, _) = countOverlap grid diag -- Part 2: count additional diagonal lines
+  let graphedLines = sort $ parseInput input
+  let (p1Points, p2Points) = getSortedPartPoints graphedLines
+  let p1 = length $ filter (\x -> length x > 1) $ group p1Points
+  let p2 = length $ filter (\x -> length x > 1) $ group p2Points
   (Just $ show p1, Just $ show p2)
 
 {- Input parsing functions -}
@@ -40,6 +35,7 @@ runDay05 filename = do
 -- as per the spec https://adventofcode.com/2021/day/5
 parseInput :: [T.Text] -> [Line]
 parseInput = map parseLine
+
 
 -- | 'parseLine' parses a single line of 'T.Text' into a 'Line'
 parseLine :: T.Text -> Line
@@ -56,44 +52,28 @@ parsePoint point = (parsedX, parsedY)
         (parsedX, _) = fromRight (0, "") $ R.decimal x
         (parsedY, _)  = fromRight (0, "") $ R.decimal y
 
--- | 'createGrid' creates an initial 'Grid' of all zeros of the size nessecary to graph
--- all lines in the list of 'Line' provided
-createGrid :: [Line] -> Grid
-createGrid ls = [[0 | _ <- [0..maxX]] | _ <- [0..maxY]]
-  where (maxX, maxY) = findMax ls
 
--- | 'countOverlap' graphs the list of 'Line' given onto 'Grid' and returns a pair the count of all tiles
--- that contain an overlap and the marked 'Grid'
-countOverlap :: Grid -> [Line] -> (Int, Grid)
-countOverlap initGrid ls  = (countGrid (> 1) grid, grid)
-  where points = foldl (\acc l -> acc ++ listPoints l) [] ls
-        grid = foldl markPoint initGrid points
-        
+{- Part 1 & 2 logic functions -}
+
+-- | 'getSortedPoints' gets a list of sorted 'Point' for part 1 and and part 2
+getSortedPartPoints :: [Line] -> ([Point], [Point])
+getSortedPartPoints ls = (nonDiagP, allPoints) -- part 1 is only non diagonal and part 2 is all points
+  where (diag, nonDiag) = partition isDiag ls
+        diagP = sort $ foldl (\acc l -> acc ++ listPoints l) [] diag -- create points from diagonal lines
+        nonDiagP = sort $ foldl (\acc l -> acc ++ listPoints l) [] nonDiag -- create points from horizontal/vertical
+        allPoints = mergePoints diagP nonDiagP 
+
+-- | 'mergePoints' merges a 2 lists of 'Point' that is assumed to be pre-sorted
+mergePoints ::[Point] -> [Point] -> [Point]
+mergePoints x [] = x
+mergePoints [] y = y
+mergePoints (x:xs) (y:ys) = if x > y
+                            then y : (mergePoints xs (y:ys))
+                            else x : (mergePoints (x:xs) ys)
+
 -- | 'isDiag' returns true if the line is a diagonal
 isDiag :: Line -> Bool
 isDiag ((x1, y1), (x2, y2)) = x1 /= x2 &&  y1 /= y2 
-
--- | 'markPoint' marks a 'Point' on the given 'Grid' and returns a new 'Grid'
-markPoint :: Grid -> Point -> Grid
-markPoint g (x,y) = newG
-  where (rBefore, r:rAfter) = splitAt y g
-        (cBefore, n:cAfter) = splitAt x r
-        newRow = cBefore ++ (n + 1 : cAfter)
-        newG = rBefore ++ (newRow : rAfter)
-
--- | 'findMax' finds the maximum x and y values for the list of 'Line' given
-findMax :: [Line] -> (Int, Int)
-findMax [] = (0,0)
-findMax (((x1, y1), (x2, y2)) : xs) = (newMaxX, newMaxY)
-  where (maxX, maxY) = findMax xs
-        newMaxX = maximum [maxX, x1, x2]
-        newMaxY = maximum [maxY, y1, y2]
-
--- | 'countGrid' counts the amount of tiles in the 'Grid' that satifies the predicate of
--- type '(Int -> Bool)'
-countGrid :: (Int -> Bool) -> Grid -> Int
-countGrid predF = foldl (\acc row -> acc + countRow row) 0
-  where countRow row = length $ filter predF row
 
 -- | 'listPoints' turns a 'Line' into a list of 'Point' that the line passes through
 listPoints :: Line -> [Point]
@@ -111,3 +91,5 @@ findNextPoint cur end
   | cur > end = cur - 1
   | cur < end = cur + 1
   | otherwise = cur
+
+
